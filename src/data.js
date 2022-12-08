ff34d96efd4f003c61b867a4b74f5b6605260e52
cptntz2119 <zhangdong2119@hotmail.com>
2022-12-07 20:29:02 -0800

    edit content of about section

--- src/data.js
+++ src/data.js
@@ -7,6 +7,7 @@ import portfolio from "../src/Assets/portfolio.png";
 
 export const SKILLS = [
   { name: "ReactJS", initialRating: 4 },
+  { name: "Redux", initialRating: 4 },
   { name: "JavaScript", initialRating: 4 },
   { name: "Html", initialRating: 4 },
   { name: "CSS", initialRating: 4 },
@@ -14,23 +15,29 @@ export const SKILLS = [
   { name: "Express", initialRating: 3 },
   { name: "Unit-Testing(Jest)", initialRating: 3 },
   { name: "PostgreSQL", initialRating: 4 },
+  { name: "MongoDB", initialRating: 3 },
   { name: "Socket.io", initialRating: 4 },
   { name: "Ruby on Rails", initialRating: 4 },
   { name: "Github", initialRating: 4 },
+  { name: "AWS", initialRating: 2 },
+  { name: "JAVA", initialRating: 2 },
 ];
 
 export const PROJECTS = [
   {
     image: typingDuel,
     name: "Typing Duel",
-    technologyUsed: "ReactJS, Javascript, Ruby on Rails, Express, PostgreSQL, Socket.io",
-    description: "Typing duel is an Single Page app that allows you to practice typing and optionally compete with other users.",
+    technologyUsed:
+      "ReactJS, Javascript, Ruby on Rails, Express, PostgreSQL, Socket.io",
+    description:
+      "Typing duel is an Single Page app that allows you to practice typing and optionally compete with other users.",
     url: "https://github.com/cptntz2119/LHL_final_typing_duel",
   },
   {
     image: interviewScheduler,
     name: "interviewScheduler",
-    technologyUsed: " React, Express, Webpack, Babel, Axios, WebSockets, PostgreSQL,Storybook, Jest, Testing library",
+    technologyUsed:
+      " React, Express, Webpack, Babel, Axios, WebSockets, PostgreSQL,Storybook, Jest, Testing library",
     description: "This is an single page interview scheduling app (SPA).",
     url: "https://github.com/cptntz2119/scheduler",
   },
@@ -52,17 +59,17 @@ export const PROJECTS = [
     image: wikimap,
     name: "wikimap",
     technologyUsed: "EJS, NodeJS, Express, Ajax, SASS, PostgreSQL",
-    description: "A web app that allows users to collaboratively create maps which list multiple interested location with google map API",
+    description:
+      "A web app that allows users to collaboratively create maps which list multiple interested location with google map API",
     url: "https://github.com/cptntz2119/MyMapMarker",
   },
   {
     image: portfolio,
     name: "portfolio",
-    technologyUsed: "HTML, CSS, JS, React for front-end , and Node, Express for back-end.",
+    technologyUsed:
+      "HTML, CSS, JS, React for front-end , and Node, Express for back-end.",
     description: "A SPA portfolio web",
     url: "https://github.com/cptntz2119/Personal-website/tree/main/portfolio-website",
   },
-  
-  
 ];
 export default PROJECTS;
